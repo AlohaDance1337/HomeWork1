b19f9dc9c274f93f7ad842d96ee724838ef20164
Vladimir <voldemar.maximich.kom@gmail.com>
2023-01-22 22:00:53 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Ведите первое число");
+String stringValue = Console.ReadLine();
+int a = Convert.ToInt32(stringValue);
+
+System.Console.WriteLine("Ведите второе число");
+String stringValue1 = Console.ReadLine();
+int b = Convert.ToInt32(stringValue1);
+
+if(a>b)
+    System.Console.WriteLine($"Максимальное число из двух {a}");
+else
+    System.Console.WriteLine($"Максимальное число из двух {b}");
