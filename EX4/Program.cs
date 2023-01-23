e3f7068cec6e21e374fc6ba677f63a5de34e25e2
AlohaDance1337 <121186810+AlohaDance1337@users.noreply.github.com>
2023-01-23 14:33:14 +0300

    Add files via upload

---
+++ EX4/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Ведите число (N): ");
+String stringValue = Console.ReadLine();
+int n = Convert.ToInt32(stringValue);
+
+for(int i = 0 ; i<=n; i++)
+    if (i%2==0)
+        System.Console.WriteLine(i);
