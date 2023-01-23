f9c1ccc1984589934a68e9c5a3cdd4cff13f8b9c
AlohaDance1337 <121186810+AlohaDance1337@users.noreply.github.com>
2023-01-23 14:06:38 +0300

    Add files via upload

---
+++ EX3/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Ведите число: ");
+String stringValue = Console.ReadLine();
+int a = Convert.ToInt32(stringValue);
+
+if(a%2==0)
+    System.Console.WriteLine($"Это число является четным");
+else
+    System.Console.WriteLine($"Это число является нечетным");
